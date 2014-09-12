8adaf94b1d177165007cea3e6db96bd0054af681
jautem <jautem@free.fr>
2014-09-12 23:34:37 +0200

    init

---
+++ poclab/Bootf.java
@@ -0,0 +1,14 @@
+package poclab;
+
+import src.main.java.com.jcraft.jsch.Logger;
+
+public class Bootf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Logger Mon = new Logger;
+
+	}
+
+}
